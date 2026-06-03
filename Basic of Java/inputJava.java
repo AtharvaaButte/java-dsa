1d329b6cbc58cefa4a6a163906efd11004687247
Atharva <atharvabutte03@gmail.com>
2026-06-03 16:50:55 +0530

    Added Basic of java

---
+++ Basic of Java/inputJava.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class inputJava {
+    public static void main(String args[]){
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // float f = sc.nextFloat();
+        // double d = sc.nextDouble();
+        // char ch = sc.next().charAt(0); // till the space
+        // sc.nextLine();
+        // String s = sc.nextLine();
+        // System.out.print(n);
+        // System.out.print(f);
+        // System.out.print(d);
+        // System.out.print(ch);
+        // System.out.println(s);
+        // sc.close();
+
+        // // After primitive data type wrtie the sc.nextLine() if any inptu after that means from char and thne string
+        float f = 3.15f;
+        System.out.println("%08.3f",f);
+        
+    }
+}
